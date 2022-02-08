7fe27a058197bae57d2c6aa2af6c6b5d2b8026d0
hapi-co <hello@gethapi.me>
2022-02-08 14:19:10 +1100

    Custom nav

---
+++ customNav.js
@@ -0,0 +1,8 @@
+<!-- Nav - replace icons and logos -->
+<script type="text/javascript">
+  function updateNavDark() {
+    document.getElementById("nav_searchIcn").src = "https://global-uploads.webflow.com/5b03bf37af744c59477739eb/5f4dfe4f51bdfd06496cb683_searchIcn.svg";
+    document.getElementById("nav_logo").src = "https://global-uploads.webflow.com/5b03bf37af744c59477739eb/5b0763f42f5ed7b0fdc9b1c8_logo.png";
+    }
+  window.onload = updateNavDark;
+</script>
